325c4519c7920efa1b74f6ef63b11d8133fb0b04
sgingles <webmaster@sgingles.com>
2012-08-20 00:17:14 +0800

    update cron

--- whenever/recipes/update_crontab.rb
+++ whenever/recipes/update_crontab.rb
@@ -1,15 +1,12 @@
 # Set your application name here
 appname = "sgingles"
 
-if ['solo', 'util'].include?(node[:instance_role])
-
-  # be sure to replace "app_name" with the name of your application.
   local_user = node[:users].first
   execute "whenever" do
-    cwd "/data/#{appname}/current"
+    cwd deploy[:current_path]
     user local_user[:username]
     command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
     action :run
   end
 
-end
+
