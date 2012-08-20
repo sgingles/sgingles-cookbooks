435e4e5214d30ae1011f1091ca935707af185a53
sgingles <webmaster@sgingles.com>
2012-08-20 23:34:55 +0800

    update delayed job

--- delayed_job/recipes/deploy.rb
+++ delayed_job/recipes/deploy.rb
@@ -1,13 +1,8 @@
 node[:deploy].each do |application, deploy|
-  cron "delayed job cron for #{application}" do
-    action :create
-    minute "*/#{node[:delayed_job][:cron_interval]}"
-    hour '*'
-    day '*'
-    month '*'
-    weekday '*'
-    command "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} rake jobs:work"
-    user deploy[:user]
-    path "/usr/bin:/usr/local/bin:/bin"
+execute "delayed_job restart" do
+    cwd "#{deploy[:deploy_to]}/current"
+    command "/usr/bin/env RAILS_ENV=#{deploy[:rails_env]} #{deploy[:deploy_to]}/current/script/delayed_job restart"
+    user "deploy"
+    action :run
   end
 end
